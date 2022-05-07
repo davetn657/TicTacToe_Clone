6509090e63160add0af8a51de2393c8896a82257
Dave Ngo <davetn657@gmail.com>
2022-05-07 01:45:46 -0400

    TicTacToe Clone Implemented
    Features added:
    - Gameplay
    - Win/Lose sequence

---
+++ TicTacToe/TicTacToe/Board.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Board
+    {
+        public char[,] board = new char[3, 3];
+
+        public void PrintBoard()
+        {
+            Console.WriteLine("---------------");
+            for(int r = 0; r < board.GetLength(0); r++)
+            {
+                for(int c = 0; c < board.GetLength(1); c++)
+                {
+                    if (((int)board[r,c]).Equals(0))
+                    {
+                        Console.Write("|   |");
+                    }
+                    else
+                    {
+                        Console.Write("| " + board[r, c] + " |");
+                    }
+                    
+                }
+                Console.WriteLine("\n---------------");
+            }
+        }
+
+        public void UpdateBoard(int position, char player)
+        {
+            switch (position)
+            {
+                case int n when (n >= 0 && n <= 2):
+                    board[0, position] = player;
+                    break;
+                case int n when (n >= 3 && n <= 5):
+                    board[1, position - 3] = player;
+                    break;
+                case int n when (n >= 6 && n <= 8):
+                    board[2, position - 6] = player;
+                    break;
+            }
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board gameBoard = new Board();
+            gameBoard.PrintBoard();
+
+            List<int> occupiedPositions = new List<int>();
+            char playerSign = 'x';
+
+            while (CheckWin(gameBoard).Equals(' '))
+            {
+                Console.WriteLine("type 1-9");
+                string playerInput = Console.ReadLine();
+                bool playerInputCheck = Int32.TryParse(playerInput, out int playerPosition);
+                
+
+                if (occupiedPositions.Contains(playerPosition) || !playerInputCheck)
+                {
+                    Console.WriteLine("Please type a valid open position");
+                    gameBoard.PrintBoard();
+                    continue;
+                }
+                else
+                {
+                    gameBoard.UpdateBoard(playerPosition - 1, playerSign);
+                    occupiedPositions.Add(playerPosition);
+                }
+
+                switch (playerSign)
+                {
+                    case 'x':
+                        playerSign = 'o';
+                        break;
+                    case 'o':
+                        playerSign = 'x';
+                        break;
+                }
+
+                gameBoard.PrintBoard();
+            }
+
+            Console.WriteLine("The Winner is Player {0}!", CheckWin(gameBoard));
+            Console.WriteLine("Press Enter to Quit");
+            Console.ReadKey();
+        }
+
+        public static char CheckWin(Board gameBoard)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                //check horizontal win
+                if(!((int)gameBoard.board[i,0]).Equals(0) && gameBoard.board[i, 0].Equals(gameBoard.board[i,1]) && gameBoard.board[i, 0].Equals(gameBoard.board[i, 2]))
+                {
+                    return gameBoard.board[i, 0];
+                }
+
+                //check vertical win
+                if(!((int)gameBoard.board[0, i]).Equals(0) && gameBoard.board[0,i].Equals(gameBoard.board[1,i]) && gameBoard.board[0, i].Equals(gameBoard.board[2, i]))
+                {
+                    return gameBoard.board[0, i];
+                }
+            }
+
+            //check diagonal win
+            if(!((int)gameBoard.board[0,0]).Equals(0) && gameBoard.board[0,0].Equals(gameBoard.board[1,1]) && gameBoard.board[0, 0].Equals(gameBoard.board[2, 2]))
+            {
+                return gameBoard.board[0, 0];
+            }
+
+            if (!((int)gameBoard.board[0, 2]).Equals(0) && gameBoard.board[0, 2].Equals(gameBoard.board[1, 1]) && gameBoard.board[0, 2].Equals(gameBoard.board[2, 0]))
+            {
+                return gameBoard.board[0, 2];
+            }
+
+            return ' ';
+        }
+    }
+}
